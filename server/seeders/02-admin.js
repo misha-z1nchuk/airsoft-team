e9ef331958a04200024500a5638fcbf3bd92f1cf
misha <zinchuckmisha@ukr.net>
2022-01-10 11:50:37 +0200

    admin seed fix bag

--- server/seeders/02-admin.js
+++ server/seeders/02-admin.js
@@ -1,18 +1,19 @@
 'use strict';
-const authService = require('../services/auth-service')
+const bcrypt = require('bcrypt')
 
 module.exports = {
     up: async (queryInterface) => {
-        let res = await queryInterface.bulkInsert('user', [
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, salt)
+        await queryInterface.bulkInsert('user', [
             {
                 first_name: 'ADMIN',
                 last_name: "ADMIN",
                 email: process.env.ADMIN_EMAIL,
-                password: process.env.ADMIN_PASSWORD,
+                password: hashedPassword,
                 roleId: "3"
             }
         ]);
-        console.log(res)
     },
 
     down: async (queryInterface) => {
