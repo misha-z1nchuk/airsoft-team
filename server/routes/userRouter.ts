6f25aa4f6c6d6cad90de72645f65bea2a8058140
misha <zinchuckmisha@ukr.net>
2022-01-10 10:27:22 +0200

    admin seed

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -3,9 +3,9 @@ import {body} from "express-validator";
 const router = Router();
 const userController = require('../controllers/user-controller')
 const authMiddleware = require('../middleware/auth-middleware')
-const authAdminMiddleware = require('../middleware/auth-admin-middleware')
+const authAdminManagerMiddleware = require('../middleware/auth-manager-admin-middleware')
 
-router.get('/:id', authAdminMiddleware, userController.getUser)
+router.get('/:id', authAdminManagerMiddleware, userController.getUser)
 router.get('/get-users', authMiddleware, userController.getUsers)
 router.post('/change-img', authMiddleware, userController.changePhoto)
 router.post('/change-email', [
---
+++ server/seeders/02-admin.js
@@ -0,0 +1,21 @@
+'use strict';
+const authService = require('../services/auth-service')
+
+module.exports = {
+    up: async (queryInterface) => {
+        let res = await queryInterface.bulkInsert('user', [
+            {
+                first_name: 'ADMIN',
+                last_name: "ADMIN",
+                email: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PASSWORD,
+                roleId: "3"
+            }
+        ]);
+        console.log(res)
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.sequelize.query(`DELETE FROM "user" where "user"."email"='${process.env.ADMIN_EMAIL}'`);
+    }
+};
